63e8be4d62d927de211ce1f81e798d3b2b17155a
Hudson Bruno <hb.natal@gmail.com>
2023-09-03 18:28:18 -0300

    feat: added terrain props

--- Assets/Terrain/Scripts/ChunkManager.cs
+++ Assets/Terrain/Scripts/ChunkManager.cs
@@ -1,90 +1,39 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using Utils;
 
 public class ChunkManager : MonoBehaviour
 {
-    public Transform target;
-    public GameObject chunkTerrainPrefab;
-    public int worldDiameter = 3;
-
-    private Dictionary<Vector3, GameObject> chunks = new Dictionary<Vector3, GameObject>();
-    private Vector3 chunkSize;
-    private float offset;
-    private Vector3 lastUpdatedTargetPos = Vector3.one;
+    public float minimumDistance = 2f;
+    public float padding = 3;
+    public GameObject[] props;
+    public GameObject terrain;
 
     void Start()
     {
-        Vector3 ogChunkSize = chunkTerrainPrefab.GetComponent<MeshRenderer>().localBounds.size;
-        Vector3 ogChunkScale = chunkTerrainPrefab.transform.localScale;
+        MeshRenderer meshRenderer = terrain.GetComponent<MeshRenderer>();
 
-        chunkSize = new Vector3(ogChunkSize.x * ogChunkScale.x, ogChunkSize.y * ogChunkScale.y, ogChunkSize.z * ogChunkScale.z);
-        offset = Mathf.FloorToInt(worldDiameter / 2f);
-
-        ManageChunkGeneration();
-    }
-
-    void Update()
-    {
-        ManageChunkGeneration();
-    }
-
-    void ManageChunkGeneration()
-    {
-        float angle = Mathf.Deg2Rad * chunkTerrainPrefab.transform.rotation.eulerAngles.x;
-        float angledZOffset = chunkSize.z * MathF.Cos(angle);
-        float angledYOffset = chunkSize.z * MathF.Sin(angle);
-
-        // Player position centered related to chunks
-        Vector3 newTargetPos = new Vector3(
-            Mathf.Round(target.position.x / chunkSize.x) * chunkSize.x,
-            -Mathf.Round(target.position.z / angledZOffset) * angledYOffset,
-            Mathf.Round(target.position.z / angledZOffset) * angledZOffset
+        Vector2 topRight = new Vector2(
+            meshRenderer.localBounds.max.x * meshRenderer.transform.localScale.x - padding,
+            meshRenderer.localBounds.max.z * meshRenderer.transform.localScale.z - padding
+        );
+        Vector2 bottomLeft = new Vector2(
+            meshRenderer.localBounds.min.x * meshRenderer.transform.localScale.x + padding,
+            meshRenderer.localBounds.min.z * meshRenderer.transform.localScale.z + padding
         );
 
-        if (newTargetPos == lastUpdatedTargetPos)
+        Vector2[] projectedPoints = FastPoissonDiskSampling.Sampling(bottomLeft, topRight, minimumDistance).ToArray();
+
+        for (int i = 0; i < projectedPoints.Length; i++)
         {
-            return; 
+            Vector3 pos = new Vector3(
+                projectedPoints[i].x,
+                -Mathf.Sin(terrain.transform.rotation.eulerAngles.x * Mathf.Deg2Rad) * projectedPoints[i].y,
+                Mathf.Cos(terrain.transform.rotation.eulerAngles.x * Mathf.Deg2Rad) * projectedPoints[i].y
+            ) + transform.position;
+
+            Instantiate(props[Random.Range(0, props.Length - 1)], pos, meshRenderer.transform.rotation, transform);
         }
-
-        HashSet<Vector3> chunkKeysToDelete = chunks.Keys.ToHashSet();
-
-        // generate new chunks if necessary
-        for (int x = 0; x < worldDiameter; x++)
-        {
-            float relativeXPosition = (x - offset) * chunkSize.x;
-            for (int z = 0; z < worldDiameter; z++)
-            {
-                float relativeZPosition = (z - offset) * angledZOffset;
-                float relativeYPosition = (z - offset) * angledYOffset;
-
-                Vector3 pos = new Vector3(
-                    relativeXPosition + newTargetPos.x,
-                    newTargetPos.y - relativeYPosition,
-                    relativeZPosition + newTargetPos.z
-                );
-
-                if (!chunks.ContainsKey(pos))
-                {
-                    chunks.Add(pos, Instantiate(chunkTerrainPrefab, pos, chunkTerrainPrefab.transform.rotation, transform));
-                }
-                else
-                {
-                    chunkKeysToDelete.Remove(pos);
-                }
-            }
-        }
-        
-        // delete unneded chunks
-        foreach (var chunkToDeleteKey in chunkKeysToDelete)
-        {
-            GameObject chunkToDestroy;
-            chunks.Remove(chunkToDeleteKey, out chunkToDestroy);
-            Destroy(chunkToDestroy);
-        }
-
-        lastUpdatedTargetPos = newTargetPos;
     }
 }
