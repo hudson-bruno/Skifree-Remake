796ec9e82331def68b91debe78a23fcbfd812788
Kobiin <143976295+Kobiin@users.noreply.github.com>
2023-09-11 20:03:00 -0300

    fix: bug

--- Assets/Scripts/HostessBehaviour.cs
+++ Assets/Scripts/HostessBehaviour.cs
@@ -1,23 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HostessBehaviour : MonoBehaviour, IInteractable
-{
-    public Transform toolTipPosition;
-
-    public void Interact(GameObject interactor)
-    {
-        Debug.Log(name + " interacted!");
-    }
-    public Vector3 GetToolTipPosition()
-    {
-        return toolTipPosition.position;
-    }
-}
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.XR;
 
 public class HostessBehaviour : MonoBehaviour, IInteractable
